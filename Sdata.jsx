89473d1ce1ed5adc8553ec40be0e0acd04fc4346
Pushkar Choudhary <96918138+Pushkariiit@users.noreply.github.com>
2023-05-21 14:27:12 +0530

    Create Sdata.jsx
    This is json file stroing the contents

---
+++ Sdata.jsx
@@ -0,0 +1,45 @@
+const Sdata=[
+    {
+        sname:"DARK",
+        imgscr :"https://wallpapercave.com/wp/wp4056410.jpg",
+        title:"Netflix Original Series",
+        links:"https://www.netflix.com/browse",
+    },
+
+    {
+        sname:"extra curricular",
+        imgscr :"https://www.heavenofhorror.com/wp-content/uploads/2020/04/Extracurricular-netflix-review.jpg",
+        title:"Netflix Original Series",
+        links:"https://www.netflix.com/browse",
+    },
+
+    {
+        sname:"Stranger Things",
+        imgscr :"https://wallpapercave.com/wp/wp1917154.jpg",
+        title:"Netflix Original Series",
+        links:"https://www.netflix.com/browse",
+    },
+
+    {
+        sname:"the Vampire Diaries",
+        imgscr :"https://static.onecms.io/wp-content/uploads/sites/6/2012/02/vampire-diaries-ad_510.jpg",
+        title:"Netflix Original Series",
+        links:"https://www.netflix.com/browse5",
+    },
+
+    {
+        sname:"My First-2 Love",
+        imgscr :"https://i.pinimg.com/474x/7b/19/58/7b19584ec5ffba063a121508e78fe7a4.jpg",
+        title:"Netflix Original Series",
+        links:"https://www.netflix.com/browse",
+    },
+
+    {
+        sname:"Friends",
+        imgscr :"https://wallpaperaccess.com/full/1810115.jpg",
+        title:"Netflix Original Series",
+        links:"https://www.netflix.com/browse",
+    },
+
+];
+export default Sdata;
